1f450117a5dd908f208c7521d0508354257c1695
edwardh273 <edwardhicks273@gmail.com>
2025-09-27 11:43:29 +0100

    fixed row, col references.  Fixed print statements

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -35,7 +35,7 @@ def main():
     sqSelected = ()  # no square is selected initially.  Keeps track of last click of user (tuple: (col, row))
     playerClicks = []  # keep track of player clicks (two tuples: [(4, 7), (4, 5)])
 
-    whitePlayer = True  # if a human is playing white, then True.  If AI is playing, then false
+    whitePlayer = False  # if a human is playing white, then True.  If AI is playing, then false
     blackPlayer = False  # same as above, but for black.
 
 
@@ -97,7 +97,7 @@ def main():
 
         if gameOver:  # end of game logic
             clock.tick(5)
-            if gs.inCheck():
+            if gs.inCheck:
                 if gs.whiteToMove:
                     drawText(screen, "Black wins by checkmate")
                 else:
@@ -123,12 +123,9 @@ def main():
                     moveMade = True
                 else:  # if checkmate inevitable
                     if validMoves:
-                        print([move.moveID for move in validMoves])
                         AIMove = findRandomMove(validMoves)
                         gs.makeMove(AIMove)
                         moveMade = True
-                    else:
-                        gameOver = True
                 AIThinking = False
 
 
@@ -138,7 +135,9 @@ def main():
             print()
             print("-----White to move-----") if gs.whiteToMove else print("-----Black to move-----")
             validMoves = gs.getValidMoves()
-            moveMade = False  # set back to False
+            if not validMoves:  # if no valid moves for next turn then gameOver
+                gameOver = True
+            moveMade = False
 
 
         p.display.flip()  # updates the full display Surface to the screen.
